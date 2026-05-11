cfa9678c30fe2601a8cbd7d80ebd449e4a6ef127
Javier Minto Arze <diegominto2@gmail.com>
2026-05-11 18:20:24 -0400

    Fallo Prueba

--- app.test.js
+++ app.test.js
@@ -5,11 +5,11 @@ describe('Pruebas de API Películas', () => {
   test('GET /movies - listar películas', async () => {
     const res = await request(app).get('/movies');
     expect(res.statusCode).toBe(200);
-    expect(res.body.length).toBe(3);
+    expect(res.body.length).toBe(2);
   });
 
   test('POST /movies - crear película', async () => {
-    const res = await request(app).post('/movies').send({ movie: "Inception", category: "Sci-Fi" });
+    const res = await request(app).post('/movies').send({ movie: "Inception", categori: "Sci-Fi" });
     expect(res.statusCode).toBe(201);
   });
 });
