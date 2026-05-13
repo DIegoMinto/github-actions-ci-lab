e05ebaecd84ff78f14b8c20ada9355ea8ab33468
Javier Minto Arze <diegominto2@gmail.com>
2026-05-13 15:05:14 -0400

    feat: actualiza mensaje de health check

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const app = require('./app');
 const PORT = 3000;
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok' });
+  res.status(200).json({ status: 'ok', version: '2.0.0' });
 });
-
 app.get('/', (req, res) => {
   res.send('Servidor funcionando');
 });
