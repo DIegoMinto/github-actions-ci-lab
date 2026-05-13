ac39de94254c2c190bd82f2b525a66a9881ca8e9
Javier Minto Arze <diegominto2@gmail.com>
2026-05-13 15:07:37 -0400

    test: Prueba sin health

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = require('./app');
 const PORT = 3000;
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok', version: '2.0.0' });
-});
+//app.get('/health', (req, res) => {
+//  res.status(200).json({ status: 'ok', version: '2.0.0' });
+//});
 app.get('/', (req, res) => {
   res.send('Servidor funcionando');
 });
